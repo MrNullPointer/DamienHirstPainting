722d7eeebb66372911569223f7df617dacd3790d
MrNullPointer <58353903+MrNullPointer@users.noreply.github.com>
2020-12-10 21:43:46 -0500

    Generating random walks for the turtle

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import random
 
 hirst = Turtle()
 
-colors = ["Red", "Blue", "Green", "Yellow", "Cyan", "Pink", "Magenta", "Gray", "Purple", "Black"]
+colors = ["#2F4F4F", "#fafcc2", "#9ab3f5", "#ffa5a5", "#794c74", "#AFEEEE", "#FF6347", "#00FF00", "#48D1CC", "#ff9b93", "#4e8d7c", "#d35d6e", "#db6400", "#bce6eb", "#f05454", "#cad315", "#7e7474", "#2d6187", "#555555"]
+directions = [0, 90, 180, 270]
+
 
 def drawShapes(sides):
     hirst.color(random.choice(colors))
@@ -11,11 +13,21 @@ def drawShapes(sides):
     for _ in range(sides):
         hirst.left(angle)
         hirst.forward(100)
-        
+
+
+def randomWalk():
+    hirst.pensize(5)
+    hirst.speed(6)
+    for _ in range(500):
+        hirst.forward(30)
+        hirst.setheading(random.choice(directions))
+        hirst.color(random.choice(colors))
+
 
 def main():
-    for sides in range(3, 11):
-        drawShapes(sides)
+    #     for sides in range(3, 11):
+    #         drawShapes(sides)
+    randomWalk()
 
 
 main()
